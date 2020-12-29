59414c035f6a7468d8bbda1f5fec475462c50575
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 18:02:23 -0300

    Controllers for sockets

---
+++ src/controllers/socket/router.js
@@ -0,0 +1,16 @@
+const sockets = require('../../sockets')
+
+const router = {
+  app: {
+    'request:status': () => {
+      sockets.watcher.emit('request:status')
+    },
+  },
+  watcher: {
+    'response:status': (data) => {
+      sockets.app.emit('response:status', data)
+    }
+  }
+}
+
+module.exports = router
